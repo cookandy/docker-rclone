db72039fb74178d0f54db5864e0eec0a8f18ce80
Andy Cook <acook@plexapp.com>
2019-03-08 16:32:11 -0700

    clean up

--- root/app/rclone.sh
+++ root/app/rclone.sh
@@ -6,14 +6,16 @@
   sync_command="rclone sync /data $SYNC_DESTINATION:/'$SYNC_DESTINATION_SUBPATH'"
 
   sendPushover() {
-    curl -s \
-	  -F "token=$PUSHOVER_APP_TOKEN" \
-	  -F "user=$PUSHOVER_USER_TOKEN" \
-	  -F "sound=$PUSHOVER_SOUND" \
-	  -F "priority=1" \
-	  -F "title=$1" \
-	  -F "message=$2" \
-	  http://api.pushover.net/1/messages > /dev/null
+    if [[ "$PUSHOVER_ENABLED" == "true" ]]; then
+      curl -s \
+	    -F "token=$PUSHOVER_APP_TOKEN" \
+	    -F "user=$PUSHOVER_USER_TOKEN" \
+	    -F "sound=$PUSHOVER_SOUND" \
+	    -F "priority=1" \
+	    -F "title=$1" \
+	    -F "message=$2" \
+	    http://api.pushover.net/1/messages > /dev/null
+    fi
   }
 
   
@@ -26,20 +28,14 @@
     fi
   fi
   
-  # send pushover if enabled
-  if [[ "$PUSHOVER_ENABLED" == "true" ]]; then
-    sendPushover "Starting backup of $RCLONE_DOCKER_NAME" "running $sync_command..."
-  fi  
+  sendPushover "Starting backup of $DOCKER_NAME" "Running $(eval echo $sync_command)..." 
   
   echo "Executing => $sync_command"
   eval "$sync_command"
   
-  # send pushover if enabled
-  if [[ "$PUSHOVER_ENABLED" == "true" ]]; then
-    if [ $? -eq 0 ]; then
-      sendPushover "Successfully backed up $RCLONE_DOCKER_NAME" "Successfully backed up $RCLONE_DOCKER_NAME."
-    else
-      sendPushover "Could not back up $RCLONE_DOCKER_NAME" "Could not back up $RCLONE_DOCKER_NAME."
-    fi
+  if [ $? -eq 0 ]; then
+    sendPushover "Successfully backed up $DOCKER_NAME!" ""
+  else
+    sendPushover "Could not back up $DOCKER_NAME!" ""
   fi
 ) 200>/var/lock/rclone.lock
